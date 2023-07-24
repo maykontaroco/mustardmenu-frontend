106ef2cf350199257cd61ffd673577a54786cca3
Maykon Taroco <maykontaroco@gmail.com>
2023-07-24 20:40:06 -0300

    Created environment and updating others files

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  isProduction: false,
+  urlApi: 'http://localhost:8080',
+  basePath: '/api'
+};
