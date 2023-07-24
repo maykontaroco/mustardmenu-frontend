f06701d9d7e0e5d240f3e1b8fbe47cca370fb6a8
Maykon Taroco <maykontaroco@gmail.com>
2023-07-24 20:51:42 -0300

    Navigate to home-page after login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -6,19 +7,30 @@ import {AppComponent} from './app.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import {HomePageComponent} from './home-page/home-page.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'}, // Rota padrão para a página inicial
+  {path: 'login', component: LoginPageComponent}, // Rota para a página inicial (outra página já existente)
+  {path: 'home', component: HomePageComponent}, // Rota para a nova página
+  {path: '**', redirectTo: 'login'}, // Rota padrão para redirecionar para a página inicial caso a rota não exista
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
+    HomePageComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
 
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {LoginService} from "../services/login.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Md5} from "ts-md5";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -13,7 +14,7 @@ export class LoginPageComponent implements OnInit {
   version: string = "v1.0.0";
   loginForm: FormGroup;
 
-  constructor(private loginService: LoginService, private formBuilder: FormBuilder) {
+  constructor(private loginService: LoginService, private formBuilder: FormBuilder, private router: Router) {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
@@ -26,7 +27,7 @@ export class LoginPageComponent implements OnInit {
 
   login() {
     this.loginService.login(this.loginForm.value.username, Md5.hashStr((this.loginForm.value.password))).subscribe(response => {
-      console.log('response -----> ', response.status);
+      this.router.navigate(['/home']);
     }, err => {
       console.log(err.status);
     });
