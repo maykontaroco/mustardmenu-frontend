4111e985f70e52ebab7923b9b05902c734ae5bc9
Maykon Taroco <maykontaroco@gmail.com>
2023-09-04 19:23:45 -0300

    Create dashboard without sale

--- src/app/view/dashboard-page/dashboard-page.component.ts
+++ src/app/view/dashboard-page/dashboard-page.component.ts
@@ -9,8 +9,50 @@ import {DashboardService} from "../../services/dashboard.service";
 })
 export class DashboardPageComponent {
 
-  dashValuesToday: Dashboard[] = [];
-  dashValuesWeek: Dashboard[] = [];
+  dashValuesToday: Dashboard[] = [
+    {
+      type: 'MONEY',
+      description: 'Valor em vendas',
+      value: 0,
+    },
+    {
+      type: 'UNIT',
+      description: 'Vendas',
+      value: 0,
+    },
+    {
+      type: 'UNIT',
+      description: 'Produtos vendidos',
+      value: 0,
+    },
+    {
+      type: 'MONEY',
+      description: 'Ticket médio',
+      value: 0,
+    },
+  ];
+  dashValuesWeek: Dashboard[] = [
+    {
+      type: 'MONEY',
+      description: 'Valor em vendas',
+      value: 0,
+    },
+    {
+      type: 'UNIT',
+      description: 'Vendas',
+      value: 0,
+    },
+    {
+      type: 'UNIT',
+      description: 'Produtos vendidos',
+      value: 0,
+    },
+    {
+      type: 'MONEY',
+      description: 'Ticket médio',
+      value: 0,
+    },
+  ];
 
   constructor(private dashboardService: DashboardService) {
     this.getDashboard();
